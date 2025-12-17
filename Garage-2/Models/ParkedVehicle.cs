1cf9e82647746606e97bd487ca8157cb7769502b
dave95995 <danielv3234@outlook.com>
2025-12-17 14:10:53 +0100

    Add Vehicle class

---
+++ Garage-2/Models/ParkedVehicle.cs
@@ -0,0 +1,18 @@
+﻿namespace Garage_2.Models
+{
+	public class ParkedVehicle
+	{
+		public int Id { get; set; }
+		public VehicleType VehicleType { get; set; }
+		public string LicensePlate { get; set; }
+		public string Color { get; set; }
+		public string Manufacturer { get; set; }
+		public string Model { get; set; }
+		public int NumberOfWheels { get; set; }
+		public DateTime CheckInTime { get; set; }
+
+
+		// If this is null then the vehicle is still parked
+		public DateTime? CheckOutTime { get; set; }
+	}
+}
