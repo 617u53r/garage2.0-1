215a4a00232af7919c79c13b26c73138dcf7efa8
dave95995 <danielv3234@outlook.com>
2025-12-17 15:37:27 +0100

    Seed the database

---
+++ Garage-2/Models/SeedData.cs
@@ -0,0 +1,87 @@
+﻿using Garage_2.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage_2.Models
+{
+	public static class SeedData
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new Garage_2Context(
+				serviceProvider.GetRequiredService<
+					DbContextOptions<Garage_2Context>>()))
+			{
+				// Look for any parked vehicles.
+				if (context.ParkedVehicle.Any())
+				{
+					return;   // DB has been seeded
+				}
+				context.ParkedVehicle.AddRange(
+					new ParkedVehicle
+					{
+						VehicleType = VehicleType.Car,
+						LicensePlate = "ABC123",
+						Color = "Red",
+						Manufacturer = "Toyota",
+						Model = "Corolla",
+						NumberOfWheels = 4,
+						CheckInTime = DateTime.Now.AddHours(-2)
+					},
+					new ParkedVehicle
+					{
+						VehicleType = VehicleType.Motorcycle,
+						LicensePlate = "XYZ789",
+						Color = "Blue",
+						Manufacturer = "Honda",
+						Model = "CBR500R",
+						NumberOfWheels = 2,
+						CheckInTime = DateTime.Now.AddHours(-1)
+					},
+					new ParkedVehicle
+					{
+						VehicleType = VehicleType.Truck,
+						LicensePlate = "LMN456",
+						Color = "White",
+						Manufacturer = "Ford",
+						Model = "F-150",
+						NumberOfWheels = 4,
+						CheckInTime = DateTime.Now.AddHours(-3)
+					},
+					new ParkedVehicle
+					{
+						VehicleType = VehicleType.Bus,
+						LicensePlate = "BUS321",
+						Color = "Yellow",
+						Manufacturer = "Mercedes-Benz",
+						Model = "Citaro",
+						NumberOfWheels = 6,
+						CheckInTime = DateTime.Now.AddHours(-4)
+					},
+					new ParkedVehicle
+					{
+						VehicleType = VehicleType.Car,
+						LicensePlate = "DEF456",
+						Color = "Black",
+						Manufacturer = "BMW",
+						Model = "3 Series",
+						NumberOfWheels = 4,
+						CheckInTime = DateTime.Now.AddHours(-5)
+					},
+					new ParkedVehicle
+					{
+						VehicleType = VehicleType.Motorcycle,
+						LicensePlate = "GHI789",
+						Color = "Green",
+						Manufacturer = "Yamaha",
+						Model = "MT-07",
+						NumberOfWheels = 2,
+						CheckInTime = DateTime.Now.AddHours(-6)
+					}
+
+				);
+				context.SaveChanges();
+			}
+		}
+	}
+
+}
--- Garage-2/Program.cs
+++ Garage-2/Program.cs
@@ -1,21 +1,31 @@
+using Garage_2.Data;
+using Garage_2.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Garage_2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Garage_2Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Garage_2Context") ?? throw new InvalidOperationException("Connection string 'Garage_2Context' not found.")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("Garage_2Context") ?? throw new InvalidOperationException("Connection string 'Garage_2Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Populate the database
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+
+	SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -26,9 +36,16 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=ParkedVehicles}/{action=Index}/{id?}")
-    .WithStaticAssets();
+	name: "default",
+	pattern: "{controller=ParkedVehicles}/{action=Index}/{id?}")
+	.WithStaticAssets();
 
+// Create the database and apply migrations at startup
+// not used in production scenarios
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<Garage_2Context>();
+	db.Database.Migrate();
+}
 
 app.Run();
