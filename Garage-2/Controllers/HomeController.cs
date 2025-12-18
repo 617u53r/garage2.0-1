a9da3be04caabba24d328b1197282578847dbc60
louai20 <alayoubi@kth.se>
2025-12-18 08:05:36 +0100

    receipt temp

--- Garage-2/Controllers/HomeController.cs
+++ Garage-2/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-using System.Diagnostics;
 using Garage_2.Models;
+using Garage_2.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace Garage_2.Controllers
 {
@@ -16,10 +18,64 @@ namespace Garage_2.Controllers
             return View();
         }
 
+        public IActionResult Receipt(int id)
+        {
+            var vehicle = _vehicles.FirstOrDefault(v => v.Id == id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            var checkoutTime = DateTime.Now;
+
+            var vm = new ReceiptViewModel
+            {
+                RegistrationNumber = vehicle.LicensePlate,
+                VehicleType = vehicle.VehicleType.ToString(),
+                CheckInTime = vehicle.CheckInTime,
+                CheckOutTime = checkoutTime,
+                TotalParkedTime = checkoutTime - vehicle.CheckInTime,
+                Price = CalculatePrice(vehicle.CheckInTime, checkoutTime)
+            };
+
+            return View(vm);
+        }
+        private static List<ParkedVehicle> _vehicles = new List<ParkedVehicle>
+        {
+            new ParkedVehicle
+            {
+                Id = 1,
+                LicensePlate = "ABC123",
+                VehicleType = VehicleType.Car,
+                Color = "Red",
+                Manufacturer = "Toyota",
+                Model = "Corolla",
+                NumberOfWheels = 4,
+                CheckInTime = DateTime.Now.AddHours(-2) // 2 hours ago
+            },
+            new ParkedVehicle
+            {
+                Id = 2,
+                LicensePlate = "XYZ789",
+                VehicleType = VehicleType.Motorcycle,
+                Color = "Blue",
+                Manufacturer = "Honda",
+                Model = "CB500",
+                NumberOfWheels = 2,
+                CheckInTime = DateTime.Now.AddMinutes(-90) // 1.5 hours ago
+            }
+        };
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private decimal CalculatePrice(DateTime checkIn, DateTime checkOut)
+        {
+            var totalHours = Math.Ceiling((checkOut - checkIn).TotalHours);
+            return (decimal)totalHours * 20; // example: 20 SEK/hour
         }
     }
 }
---
+++ Garage-2/Models/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage_2.Models.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public required string RegistrationNumber { get; set; }
+        public required string VehicleType { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public TimeSpan TotalParkedTime { get; set; }
+        public decimal Price { get; set; }
+    }
+}
