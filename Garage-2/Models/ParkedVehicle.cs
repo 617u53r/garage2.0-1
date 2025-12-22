e768185b6810221aad333744ebaa7c5fbc539470
dave95995 <danielv3234@outlook.com>
2025-12-22 10:00:59 +0100

    Add-Migration UpdateParkedVehicleValidation

--- Garage-2/Models/ParkedVehicle.cs
+++ Garage-2/Models/ParkedVehicle.cs
@@ -1,18 +1,55 @@
-﻿namespace Garage_2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2.Models
 {
 	public class ParkedVehicle
 	{
 		public int Id { get; set; }
+
+
+		[Required]
+		[Display(Name = "Vehicle Type")]
+
 		public VehicleType VehicleType { get; set; }
-		public string LicensePlate { get; set; }
-		public string Color { get; set; }
-		public string Manufacturer { get; set; }
-		public string Model { get; set; }
+
+
+
+		[Required]
+		[StringLength(20, MinimumLength = 1)]
+		[Display(Name = "License Plate")]
+
+		public string LicensePlate { get; set; } = string.Empty;
+
+
+		[Required]
+		[StringLength(20, MinimumLength = 1)]
+		public string Color { get; set; } = string.Empty;
+
+
+		[Required]
+		[StringLength(20, MinimumLength = 1)]
+		public string Manufacturer { get; set; } = string.Empty;
+
+
+		[Required]
+		[StringLength(20, MinimumLength = 1)]
+		public string Model { get; set; } = string.Empty;
+
+
+
+
+		[Range(1, 18, ErrorMessage = "Number of wheels must be between 1 and 18.")]
+		[Display(Name = "Wheels")]
 		public int NumberOfWheels { get; set; }
+
+
+		[Display(Name = "Parked started")]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
 		public DateTime CheckInTime { get; set; }
 
 
-		// If this is null then the vehicle is still parked
+		[Display(Name = "Parked ended")]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
 		public DateTime? CheckOutTime { get; set; }
 	}
 }
